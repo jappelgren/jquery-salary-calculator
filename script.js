6279ff3c61864b0d7fb1b7f019b3443bf4b82943
Justin Appelgren <justinappelgren@gmail.com>
2020-12-12 13:23:46 -0600

    more styling changes. Added check to make sure all fields are filled out before data can be submitted.  Added an error popup if not.

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ let employees = []
 let salaryTotal = 0
 
 function onReady() {
-    $('#submit-button').on('click', addEmp)
+    $('#error-box').hide()
+    $('#submit-button').on('click', emptyFieldDetector)
     $('tbody').on('click', '.delete-button', delEmp)
-}
+} //end onReady
+
+function emptyFieldDetector() {
+    if ($('#first-name-in').val() === '' || $('#last-name-in').val() === '' || $('#id-in').val() === '' || $('#title-in').val() === '' || $('#salary-in').val() === '') {
+        $('#error-box').fadeIn(300, 'linear').delay(5000).fadeOut(300, 'linear')
+    } else {
+        $('#error-box').fadeOut(300, 'linear')
+        addEmp()
+    }
+} //end emptyFieldDetector
 
 function addEmp() {
     employees.push(
@@ -20,7 +30,7 @@ function addEmp() {
     salaryTotal += Number($('#salary-in').val());
     $('input').val('');
     displayEmp();
-}
+} //end adddEmp
 
 function displayEmp() {
     $('tbody').empty();
@@ -41,7 +51,7 @@ function displayEmp() {
         $('#total-monthly-cost').append(`<h1>Total Monthly: $${Number.parseFloat(salaryTotal).toFixed(2)}</h1>`);
     }
 
-}
+} // end displayEmp 
 
 function delEmp() {
     let employeeIndex = $(this).val();
@@ -50,5 +60,4 @@ function delEmp() {
     employees.splice(employeeIndex, 1)
 
     displayEmp()
-}
-
+} //end delEmp
